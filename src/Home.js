488bda541a19417272f47d2e209b95c5bfc945f2
SMinhChau <minhchaunguyenmtp@gmail.com>
2022-11-09 14:34:10 +0700

    screen 1

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,54 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import images from "../assets/index";
+
+import Icon from "react-native-vector-icons/FontAwesome";
 
 function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.viewImg}>
         <Image style={styles.logo} source={{ uri: images.background }} />
+      </View>
+      <View style={styles.content_top}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Discover premium coffee around you</Text>
+        </View>
+      </View>
+      <View style={[styles.content_bottom, styles.center_center]}>
+        <TouchableOpacity
+          style={[
+            styles.btn_content,
+            styles.center_center,
+            { flexDirection: "row" },
+          ]}
+        >
+          <Icon name="facebook-square" color="blue" size={30} />
+          <Text style={styles.text_btn}>Sing in with Facebook</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.btn_content,
+            styles.center_center,
+            { flexDirection: "row" },
+          ]}
+        >
+          <Icon name="google-plus" color="blue" size={30} />
+          <Text style={styles.text_btn}>Sing in with Google</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.title_center}>Don't have an account?</Text>
+        <TouchableOpacity>
+          <Text style={styles.title_bottom}>Register</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -17,20 +59,78 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
     flexDirection: "column",
-    justifyContent: "flex-start",
+    justifyContent: "space-between",
   },
 
   viewImg: {
-    backgroundColor: "#000",
     height: "50%",
     width: "100%",
     resizeMode: "contain",
+    justifyContent: "flex-end",
   },
   logo: {
-    height: 300,
+    height: 400,
     resizeMode: "contain",
+  },
+  content_top: {
+    width: "80%",
+    height: "15%",
+  },
+  content: {
+    width: "60%",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  content_bottom: {
+    height: "35%",
+    width: "100%",
+    justifyContent: "space-around",
+  },
+  center_center: {
+    display: "flex",
+
+    alignItems: "center",
+  },
+  btn: {
+    width: "80%",
+
+    paddingVertical: 10,
+  },
+  btn_content: {
+    width: "85%",
+    height: 55,
+    paddingVertical: 10,
+    borderColor: "#808000",
+    justifyContent: "center",
+    borderWidth: 2,
+    borderRadius: "10%",
+  },
+  title: {
+    fontSize: 30,
+    color: "#fff",
+  },
+  title: {
+    fontSize: 35,
+    color: "#fff",
+  },
+  title_center: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  title_bottom: {
+    fontSize: 24,
+    color: "yellow",
+    textDecorationLine: "underline",
+    textTransform: "uppercase",
+  },
+  text_btn: {
+    fontSize: 22,
+    paddingLeft: 10,
+    color: "#fff",
+    fontWeight: "500",
+    textTransform: "uppercase",
   },
 });
